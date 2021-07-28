f2d6d4577d11fba149e9386ff34376ad3d5cadce
dhruv <kadamdhruv98@gmail.com>
2021-07-28 11:02:17 -0500

    This script gets the headers of whatever is searched result listed and then prints those.

--- main.py
+++ main.py
@@ -4,6 +4,33 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-car = input("Input:")
-print(car)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#!/usr/bin/env python
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.Firefox()
+driver.get('http://techwithtim.net')
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+time.sleep(3)
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main" ))
+
+    )
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+
+finally:
+    time.sleep(3)
+    driver.quit()
+
